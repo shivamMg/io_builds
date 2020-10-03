c9a9fd1951927342ed71188857b4fa024976f087
Shivam Mamgain <shivamMg@users.noreply.github.com>
2020-10-03 17:09:15 +0530

    Read io_builds.yml

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
+import yaml
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    my_input = os.getenv('INPUT_IO_BUILD_FILE', 'io_builds.yml')
 
-    my_output = f"Hello {my_input}"
+    my_output = "Hello {my_input}".format(my_input=my_input)
 
-    print(f"::set-output name=myOutput::{my_output}")
+    print("::set-output name=myOutput::{my_output}".format(my_output=my_output))
+    with open('./io_builds.yml') as f:
+        print(yaml.safe_load(f))
 
 
 if __name__ == "__main__":
